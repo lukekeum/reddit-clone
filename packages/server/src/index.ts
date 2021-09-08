4983f99515612a78d3ce475707432980d8a91328
lukekeum <lukekeum11@gmail.com>
2021-09-08 23:02:57 +0900

    feat(server): Setup winston logger

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,10 +2,20 @@ import './utils/env'
 import 'reflect-metadata'
 import { Server } from './server'
 import { Database } from './database'
+import logger from './utils/logger'
 
 const server = new Server()
 const database = new Database()
 
-void database.connect().then(() => {
-  server.listen()
-})
+void database
+  .connect()
+  .then(() => {
+    logger.info('Database connected')
+    void server.listen().then((server) => {
+      const { port } = server.address() as { port: number }
+      logger.info(`Server started on port ${port}`)
+    })
+  })
+  .catch((reason) => {
+    throw new Error(`Databse Error: ${reason as string}`)
+  })
---
+++ packages/server/src/utils/logger.ts
@@ -0,0 +1,40 @@
+import { createLogger, transports, format } from 'winston'
+
+const { NODE_ENV } = process.env
+
+interface TransformInfo {
+  level: string
+  message: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any
+}
+
+const enumerateErrorFormat = format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack })
+  }
+  return info
+})
+
+const logger = createLogger({
+  level: NODE_ENV === 'development' ? 'debug' : 'info',
+  format: format.combine(
+    enumerateErrorFormat(),
+    NODE_ENV === 'development' ? format.colorize() : format.uncolorize(),
+    format.splat(),
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    format.printf(
+      (info: TransformInfo) =>
+        `${info.timestamp as string} ${info.level}: ${info.message}`
+    )
+  ),
+  transports: [
+    new transports.Console({
+      stderrLevels: ['error'],
+    }),
+  ],
+})
+
+export default logger
