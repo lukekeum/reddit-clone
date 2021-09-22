668341bd17f46c75c066fbb716f4bc4a025daffa
lukekeum <lukekeum11@gmail.com>
2021-09-23 00:56:36 +0900

    feat(server): Register typeorm container

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,15 @@
 import './utils/env'
 import 'reflect-metadata'
+import { useContainer } from 'typeorm'
+import { Container } from 'typeorm-typedi-extensions'
 import { Server } from './server'
 import { Database } from './database'
-import { Container } from 'typedi'
 import logger from './utils/logger'
 
-const server = Container.get(Server)
-const database = Container.get(Database)
+useContainer(Container)
+
+const server = new Server()
+const database = new Database()
 
 void database
   .connect()
