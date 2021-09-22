a3db1ddd519e367162d84da00416f16c4ab48b52
lukekeum <lukekeum11@gmail.com>
2021-09-23 00:59:27 +0900

    feat(server): Add graphQLCall function generic

--- packages/server/src/utils/tests/tools.ts
+++ packages/server/src/utils/tests/tools.ts
@@ -1,17 +1,20 @@
-/* eslint-disable @typescript-eslint/unbound-method */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 import generateSchema from '@src/graphql/schema'
-import { graphql, GraphQLSchema } from 'graphql'
+import { ExecutionResult, graphql, GraphQLSchema } from 'graphql'
 import { ASTNode, print as gqlToString } from 'graphql/language'
 import { Maybe } from 'graphql/jsutils/Maybe'
+
+interface Context {
+  req: Record<string, unknown>
+  res: Record<string, unknown>
+  [key: string]: unknown
+}
 
 interface GraphQLCallOptions {
   source: string | ASTNode
   variables?: Maybe<{
     [key: string]: unknown
   }>
+  context?: Partial<Context>
 }
 
 class GraphQLTesting {
@@ -22,18 +25,22 @@ class GraphQLTesting {
   }
 
   async initializeSchema() {
-    this.schema = await generateSchema()
+    this.schema = await generateSchema(true)
   }
 
-  async graphQLCall({ source, variables }: GraphQLCallOptions) {
+  graphQLCall = async <T>({
+    source,
+    variables,
+    context,
+  }: GraphQLCallOptions): Promise<ExecutionResult<T>> => {
     if (!this.schema) await this.initializeSchema()
 
     return graphql({
       schema: this.schema,
       source: typeof source === 'string' ? source : gqlToString(source),
       variableValues: variables,
-    })
+      contextValue: context,
+    }) as ExecutionResult<T>
   }
 }
-
 export const graphQLCall = new GraphQLTesting().graphQLCall
