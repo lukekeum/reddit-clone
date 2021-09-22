75839eebd41fb93565071164cdbca89f817b8539
lukekeum <lukekeum11@gmail.com>
2021-09-23 00:59:00 +0900

    test(server): Add login/register test

---
+++ packages/server/src/graphql/user/__tests__/login.test.ts
@@ -0,0 +1,100 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { User } from '@src/entities/user/User'
+import { gql } from 'apollo-server-core'
+import faker from 'faker'
+import { UserRepository } from '@src/entities/user/UserRepository'
+import { getCustomRepository } from 'typeorm'
+import { graphQLCall } from '@src/utils/tests/tools'
+import { v4 as uuidv4 } from 'uuid'
+import { TokenResponse } from '../UserResolver'
+import { isJWT } from 'class-validator'
+import { verify } from 'jsonwebtoken'
+import { ACCESS_TOKEN_SECRET } from '@src/contants'
+
+describe('로그인', () => {
+  const LOGIN_USER = gql`
+    mutation ($username: String!, $password: String!) {
+      login(input: { username: $username, password: $password }) {
+        user_id
+        accessToken
+      }
+    }
+  `
+
+  let mockUser: User
+  let userData: Partial<User>
+
+  beforeEach(async () => {
+    const userRepository = getCustomRepository(UserRepository)
+
+    userData = {
+      id: uuidv4(),
+      email: faker.internet.email(),
+      username: faker.internet.userName(),
+      password: faker.internet.password(),
+    }
+
+    mockUser = await userRepository.create(userData).save()
+  })
+
+  afterEach(async () => {
+    const userRepository = getCustomRepository(UserRepository)
+
+    await userRepository.delete(mockUser)
+  })
+
+  it('올바르지 않은 유저이름 값', async () => {
+    const user = await graphQLCall({
+      source: LOGIN_USER,
+      variables: { username: 'wrong', password: mockUser.password },
+    })
+
+    expect(user.errors).toBeDefined()
+  })
+
+  it('올바르지 않은 비밀번호', async () => {
+    const user = await graphQLCall({
+      source: LOGIN_USER,
+      variables: { username: mockUser.username, password: 'wrong' },
+    })
+
+    expect(user.errors).toBeDefined()
+  })
+
+  it('올바르지 않은 유저이름 값과 비밀번호', async () => {
+    const user = await graphQLCall({
+      source: LOGIN_USER,
+      variables: { username: 'wrong', password: 'wrong' },
+    })
+
+    expect(user.errors).toBeDefined()
+  })
+
+  it('유저 로그인', async () => {
+    const user = mockUser
+
+    const response = await graphQLCall<{ login: TokenResponse }>({
+      source: LOGIN_USER,
+      variables: { username: user.username, password: userData.password },
+      context: {
+        res: {
+          cookie: jest.fn(),
+        },
+      },
+    })
+
+    expect(response.data).toBeDefined()
+
+    const { accessToken, user_id } = response.data!.login
+
+    expect(response.data).toBeDefined()
+    expect(isJWT(accessToken)).toBe(true)
+    expect(user_id).toBe(mockUser.id)
+
+    const payload = verify(accessToken, ACCESS_TOKEN_SECRET) as {
+      userId: string
+    }
+
+    expect(payload.userId).toBe(mockUser.id)
+  })
+})
---
+++ packages/server/src/graphql/user/__tests__/register.test.ts
@@ -0,0 +1,88 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { UserRepository } from '@src/entities/user/UserRepository'
+import { graphQLCall } from '@src/utils/tests/tools'
+import { gql } from 'apollo-server-core'
+import { getCustomRepository } from 'typeorm'
+import faker from 'faker'
+
+interface RegisterMutationData {
+  register: boolean
+}
+
+describe('회원가입', () => {
+  const REGISTER_USER = gql`
+    mutation ($email: String!, $username: String!, $password: String!) {
+      register(
+        input: { email: $email, username: $username, password: $password }
+      )
+    }
+  `
+
+  it('올바르지 않은 이메일 값', async () => {
+    const email = 'wrong' // 올바르지 않은 이메일 형식
+    const username = faker.internet.userName()
+    const password = faker.internet.password()
+
+    const response = await graphQLCall({
+      source: REGISTER_USER,
+      variables: { email, username, password },
+    })
+
+    expect(response.data).toBe(null)
+    expect(response.errors).toBeDefined()
+  })
+
+  it('이미 존재하는 유자', async () => {
+    const userRepository = getCustomRepository(UserRepository)
+
+    const email = faker.internet.email()
+    const username = faker.internet.userName()
+    const password = faker.internet.password()
+
+    const user = userRepository.create({
+      email,
+      username,
+      password,
+    })
+
+    await user.save()
+
+    const response = await graphQLCall({
+      source: REGISTER_USER,
+      variables: { email, username, password },
+    })
+
+    expect(response.data).toBe(null)
+    expect(response.errors).toBeDefined()
+  })
+
+  it('유저 회원가입', async () => {
+    const userRepository = getCustomRepository(UserRepository)
+
+    const email = faker.internet.email()
+    const username = faker.internet.userName()
+    const password = faker.internet.password()
+
+    const user = await userRepository.create({
+      email,
+      username,
+      password,
+    })
+
+    const response = await graphQLCall<RegisterMutationData>({
+      source: REGISTER_USER,
+      variables: { email, username, password },
+      context: {
+        res: {},
+      },
+    })
+
+    expect(response.data!.register).toBe(true)
+
+    const userFromDB = await userRepository.findOneByEmail(email)
+
+    expect(userFromDB).toBeDefined()
+    expect(userFromDB?.email === user.email).toEqual(true)
+    expect(userFromDB?.username === user.username).toEqual(true)
+  })
+})
