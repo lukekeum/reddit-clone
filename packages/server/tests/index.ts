807e6f94b92f962ab51e02cb96ca5b68d47dbe0f
lukekeum <lukekeum11@gmail.com>
2021-09-23 00:59:50 +0900

    test(server): Add --runInBand option

--- packages/server/tests/index.ts
+++ packages/server/tests/index.ts
@@ -3,4 +3,4 @@ import * as jest from 'jest'
 const argv = process.argv.slice(2)
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-jest.run(argv)
+jest.run(['--runInBand', ...argv])
