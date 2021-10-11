72952fac1437c266950440c97bddd471c4ad800a
lukekeum <lukekeum11@gmail.com>
2021-10-12 02:26:54 +0900

    feat(server): Add vote find method

--- packages/server/src/entities/vote/VoteRepository.ts
+++ packages/server/src/entities/vote/VoteRepository.ts
@@ -14,4 +14,15 @@ export class VoteRepository extends Repository<Vote> {
 
     return parseInt(voteNum.sum, 10) || 0
   }
+
+  public async findVoteByPostIdAndUserId(
+    post_id: string,
+    user_id: string
+  ): Promise<Vote | undefined> {
+    const vote = await this.createQueryBuilder('vote')
+      .where('vote.fk_post_id = :fk_post_id', { fk_post_id: post_id })
+      .andWhere('vote.fk_user_id = :fk_user_id', { fk_user_id: user_id })
+      .getOne()
+    return vote
+  }
 }
